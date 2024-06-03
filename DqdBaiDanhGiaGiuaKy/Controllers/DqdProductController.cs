8dce4a65bee40fb3bcc76a8b31063d4cfb44169c
DungP23 <dangquangdung24082004@gmail.com>
2024-06-03 11:12:33 +0700

    DqdBaiKiemTraGiuaKy

---
+++ DqdBaiDanhGiaGiuaKy/Controllers/DqdProductController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DqdBaiDanhGiaGiuaKy.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DqdBaiDanhGiaGiuaKy.Controllers
+{
+    public class DqdProductController : Controller
+    {
+        private static List<DqdProduct> dqdProduct = new List<DqdProduct>()
+        {
+            new DqdProduct{Id=100, Name="DangQuangDung", Image="K22Cntt1", Quantity=19, Price=2004, isActive="Active"},
+            new DqdProduct{Id=101, Name="NguyenKhanhPhuong",Image="K72Cntt1", Quantity=18, Price=2005, isActive="NonActive"}
+
+        };
+        // GET: DqdProduct
+        public ActionResult Index()
+        {
+            return View(dqdProduct);
+        }
+
+        public ActionResult DqdCreate()
+        {
+            var dqdProduct = new DqdProduct();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DqdCreate(DqdProduct dqdProduct)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(dqdProduct);
+            }
+            dqdProduct.Add(dqdProduct);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DqdBaiDanhGiaGiuaKy/Models/DqdProduct.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DqdBaiDanhGiaGiuaKy.Models
+{
+    public class DqdProduct
+    {
+        [DisplayName(" DQD- Tieu De")]
+        [Required(ErrorMessage = "DQD- Hay Nhap Tieu De")]
+        public int Id { get; set; }
+
+       
+
+        [DisplayName(" DQD- Tên")]
+        [Required(ErrorMessage = " DQD- Vui lòng nhập tên")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Tên phải dài từ 5 đến 100 ký tự")]
+        [RegularExpression(@"^[A-ZÀÁÂẨÃÈÉÊẾËÌÍĨỊÒÓÔÕÖÙÚŨỤƯỲÝỶỸ\s]+$", ErrorMessage = "Tên chỉ chứa ký tự viết hoa và khoảng trắng")]
+        public string Name { get; set; }
+
+        
+
+        [DisplayName(" DQD- Ảnh")]
+        [Required(ErrorMessage = "DQD- Hãy Nhập ẢNh")]
+        public string Image { get; set; }
+
+        
+
+        [DisplayName(" DQD- Số")]
+        [Required(ErrorMessage = " DQD- Vui lòng nhập số")]
+        [Range(1, 100, ErrorMessage = "Số phải nằm trong khoảng từ 1 đến 100")]
+        public int Quantity { get; set; }
+
+        
+
+        [DisplayName(" DQD- Số")]
+        [Required(ErrorMessage = "Vui lòng nhập số")]
+        public int Price { get; set; }
+
+        
+        [DisplayName(" DQD- Hoat Dong")]
+        [Required(ErrorMessage = " DQD- Vui lòng Nhap Hoat Dong")]
+        public string isActive { get; set; }
+
+    }
+}
